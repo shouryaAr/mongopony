f435314d581c1360101617f4aa84d0f1ae490290
Colin Howe <colinthehowe@gmail.com>
2013-10-23 15:36:54 +0000

    Small refactoring to allow overrides

--- mongopony/collection.py
+++ mongopony/collection.py
@@ -14,12 +14,20 @@ class Collection(object):
         coll = cls._get_collection(db)
 
         if hasattr(instance, 'id'):
-            dic = cls.document_strategy.object_to_dict(instance)
-            dic['_id'] = instance.id
-            coll.save(dic)
-            return instance
+            return cls._save_existing(coll, instance)
         else:
-            dic = cls.document_strategy.object_to_dict(instance)
-            new_id = coll.save(dic)
-            instance.id = new_id
-            return instance
+            return cls._save_new(coll, instance)
+
+    @classmethod
+    def _save_existing(cls, coll, instance):
+        dic = cls.document_strategy.object_to_dict(instance)
+        dic['_id'] = instance.id
+        coll.save(dic)
+        return instance
+
+    @classmethod
+    def _save_new(cls, coll, instance):
+        dic = cls.document_strategy.object_to_dict(instance)
+        new_id = coll.save(dic)
+        instance.id = new_id
+        return instance
